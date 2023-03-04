776b0f98b24e60f71db113c1c04631850392a995
Marcos Jr <marcospojr13@gmail.com>
2023-03-03 21:29:27 -0300

    Initial Commit

---
+++ SwiftUIMVVM/SwiftUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMApp.swift
+//  SwiftUIMVVM
+//
+//  Created by Marcos Jr on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

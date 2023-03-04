9efb203d65c712fee142cdf7351243440fd55bf6
Marcos Jr <marcospojr13@gmail.com>
2023-03-03 22:21:40 -0300

    Rename LandingView and add background with image
    Image generated using DALL-E IA (openia)
    https://labs.openai.com/

---
+++ SwiftUIMVVM/LandingView.swift
@@ -0,0 +1,22 @@
+//
+//  LandingView.swift
+//  SwiftUIMVVM
+//
+//  Created by Marcos Jr on 03/03/23.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        VStack {
+            Text("LandingPage")
+        }.background(Image("backgroundImage"))
+    }
+}
+
+struct LandingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandingView()
+    }
+}
--- SwiftUIMVVM/SwiftUIMVVMApp.swift
+++ SwiftUIMVVM/SwiftUIMVVMApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIMVVMApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LandingView()
         }
     }
 }
